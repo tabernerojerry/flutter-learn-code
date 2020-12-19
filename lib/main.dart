29b8afe9481bcfa8f45a37721713063f16d52cef
Jerry Tabernero <tabernerojerry@gmail.com>
2020-12-19 16:51:51 +0800

    setup sidebarand added assets to pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learncode/components/sidebar_row.dart';
+import 'package:learncode/model/sidebar.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,42 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Row(
-            children: [
-              Container(
-                width: 42.0,
-                height: 42.0,
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14.0),
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color(0xFF00AEFF),
-                      Color(0xFF0076FF),
-                    ],
-                  ),
-                ),
-                child: Icon(
-                  Icons.home,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(width: 12),
-              Container(
-                child: Text(
-                  "Home",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w800,
-                    color: Color(0xFF242629),
-                  ),
-                ),
-              ),
-            ],
+          child: SidebarRow(
+            item: sidebarItems[0],
           ),
         ),
       ),
