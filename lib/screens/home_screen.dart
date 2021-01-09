d96970992495955cc6cfdfb429bcf322aa916ac8
Jerry Tabernero <tabernerojerry@gmail.com>
2021-01-09 16:56:20 +0800

    course screen hero widget animation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -64,101 +64,103 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: kBackgroundColor,
-        child: Stack(
-          children: [
-            SafeArea(
-              child: Column(
-                children: [
-                  HomeScreenNavBar(
-                    triggerAnimation: () {
-                      setState(() {
-                        sidebarHidden = !sidebarHidden;
-                      });
-
-                      sidebarAnimationController.forward();
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          "Recents",
-                          style: kLargeTitleStyle,
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          "23 courses, more coming",
-                          style: kSubtitleStyle,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  RecentCourseList(),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 20.0,
-                      right: 20.0,
-                      top: 25.0,
-                      bottom: 16.0,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          "Explore",
-                          style: kTitle1Style,
-                        ),
-                      ],
-                    ),
-                  ),
-                  ExploreCourseList(),
-                ],
-              ),
-            ),
-            ContinueWatchingScreen(),
-            IgnorePointer(
-              ignoring: sidebarHidden,
-              child: Stack(
-                children: [
-                  FadeTransition(
-                    opacity: fadeAnimation,
-                    child: GestureDetector(
-                      child: Container(
-                        color: Color.fromRGBO(36, 38, 41, 0.4),
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                      ),
-                      onTap: () {
+      body: SingleChildScrollView(
+        child: Container(
+          color: kBackgroundColor,
+          child: Stack(
+            children: [
+              SafeArea(
+                child: Column(
+                  children: [
+                    HomeScreenNavBar(
+                      triggerAnimation: () {
                         setState(() {
                           sidebarHidden = !sidebarHidden;
                         });
 
-                        sidebarAnimationController.reverse();
+                        sidebarAnimationController.forward();
                       },
                     ),
-                  ),
-                  SlideTransition(
-                    position: sidebarAnimation,
-                    child: SafeArea(
-                      child: SidebarScreen(),
-                      bottom: false,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            "Recents",
+                            style: kLargeTitleStyle,
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Text(
+                            "23 courses, more coming",
+                            style: kSubtitleStyle,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RecentCourseList(),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 20.0,
+                        right: 20.0,
+                        top: 25.0,
+                        bottom: 16.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            "Explore",
+                            style: kTitle1Style,
+                          ),
+                        ],
+                      ),
+                    ),
+                    ExploreCourseList(),
+                  ],
+                ),
               ),
-            ),
-          ],
+              ContinueWatchingScreen(),
+              IgnorePointer(
+                ignoring: sidebarHidden,
+                child: Stack(
+                  children: [
+                    FadeTransition(
+                      opacity: fadeAnimation,
+                      child: GestureDetector(
+                        child: Container(
+                          color: Color.fromRGBO(36, 38, 41, 0.4),
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            sidebarHidden = !sidebarHidden;
+                          });
+
+                          sidebarAnimationController.reverse();
+                        },
+                      ),
+                    ),
+                    SlideTransition(
+                      position: sidebarAnimation,
+                      child: SafeArea(
+                        child: SidebarScreen(),
+                        bottom: false,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
